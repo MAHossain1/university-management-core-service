dacaf0329a2b927b481be09e514fe19e778c1cb7
MAHossain1 <mahpro110@gmail.com>
2023-08-30 17:44:15 +0600

    offered course completation done

--- src/app/modules/offeredCourse/offeredCourse.routes.ts
+++ src/app/modules/offeredCourse/offeredCourse.routes.ts
@@ -7,11 +7,27 @@ import { OfferedCourseValidation } from './offeredCourse.validation';
 
 const router = express.Router();
 
+router.get('/', OfferedCoursesController.getAllFromDB);
+router.get('/:id', OfferedCoursesController.getByIdFromDB);
+
 router.post(
   '/',
   validateRequest(OfferedCourseValidation.create),
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   OfferedCoursesController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCoursesController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCoursesController.deleteByIdFromDB
 );
 
 export const offeredCourseRoutes = router;
